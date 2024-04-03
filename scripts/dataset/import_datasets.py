8d1f5ccedb3315dcd6bbef31d0c0973faed5e07f
Matteo Pettenò <matteo.petteno1@gmail.com>
2024-04-03 12:51:30 +0200

    + PT script improvement

--- scripts/dataset/import_datasets.py
+++ scripts/dataset/import_datasets.py
@@ -1,4 +1,4 @@
-from resolv_pipelines.pipelines.datasets import ImportArchiveDatasetPipeline
+from resolv_pipelines.pipelines.datasets import ImportArchiveDatasetPipeline, CanonicalizeDatasetPipeline
 
 from scripts.utilities.constants import Paths
 
@@ -11,4 +11,22 @@ if __name__ == '__main__':
         force_overwrite=True,
         allow_invalid_checksum=False,
         logging_level="INFO"
+    ).run_pipeline()
+
+    CanonicalizeDatasetPipeline(
+        input_path=Paths.RAW_DATASETS_DIR,
+        output_path=Paths.DATASETS_DIR,
+        source_dataset_names=["lakh-midi-v1"],
+        source_dataset_modes=["clean"],
+        source_dataset_file_types=["midi"],
+        force_overwrite=True,
+        logging_level="INFO",
+        debug=False,
+        debug_file_pattern=".*",
+        pipeline_options={
+            "runner": "DirectRunner",
+            "direct_running_mode": "multi_processing",
+            "direct_num_workers": 6,
+            "direct_runner_bundle_repeat": 0
+        }
     ).run_pipeline()
--- scripts/ml/eval_power_transforms.py
+++ scripts/ml/eval_power_transforms.py
@@ -3,6 +3,7 @@ import os
 from pathlib import Path
 
 import keras
+import keras.ops as k_ops
 import matplotlib.pyplot as plt
 from resolv_mir import NoteSequence
 from resolv_ml.utilities.statistic.power_transforms import BoxCox, YeoJohnson
@@ -15,6 +16,8 @@ DATASET_FOLDER = "4bars_melodies_distinct/lakh_midi-v1.0.0-clean/midi"
 DATASET_FILE_PATTERN = 'pitchseq/pitchseq-*.tfrecord'
 DATASET_PATH = str(Paths.REPRESENTATION_DATASETS_DIR / DATASET_FOLDER / DATASET_FILE_PATTERN)
 BATCH_SIZE = 64
+BOX_COX_LAMBDA = 0.33
+YEO_JOHNSON_LAMBDA = -2.22
 HIST_OUTPUT_PATH = Paths.REPRESENTATION_DATASETS_DIR / DATASET_FOLDER / 'power_transforms'
 HISTOGRAM_BINS = [20, 30, 40, 50, 60, 70, 80]
 
@@ -36,51 +39,65 @@ def load_dataset(attribute: str):
     return tfrecord_loader.load_dataset()
 
 
-def plot_distribution(x, output_path: Path, attribute: str):
+def plot_distribution(x, output_path: Path, power_transform_id: str, lmbda: int, attribute: str):
+    output_path = output_path / power_transform_id
     output_path.mkdir(parents=True, exist_ok=True)
+    pt_title = power_transform_id.replace("_", " ").capitalize()
+    attr_title = attribute.replace("_", " ").capitalize()
     for bins in HISTOGRAM_BINS:
         plt.hist(x, bins=bins, color='blue', alpha=0.7)
-        plt.title(f'{attribute.replace("_", " ")} - {bins} bins')
-        plt.savefig(f'{str(output_path)}/{attribute}_histogram_{bins}.png', format='png', dpi=300)
+        plt.suptitle(f'{pt_title} - {attr_title}')
+        plt.title(f'lambda = {lmbda} - {bins} bins')
+        plt.savefig(f'{str(output_path)}/{attribute}_histogram_{lmbda}_{bins}.png', format='png', dpi=300)
         plt.close()
 
 
 def test_box_cox_transform(attribute: str):
     class BoxCoxModel(keras.Model):
 
+        def __init__(self, lmbda: int):
+            super(BoxCoxModel, self).__init__()
+            self._lmbda = lmbda
+
         # noinspection PyAttributeOutsideInit
         def build(self, input_shape):
-            self._box_cox_layer = BoxCox(lambda_init=0.33, batch_norm=keras.layers.BatchNormalization())
+            self._box_cox_layer = BoxCox(lambda_init=self._lmbda)
 
         def call(self, inputs):
-            return self._box_cox_layer(inputs)
+            pt_out = self._box_cox_layer(inputs)
+            pt_out_std = k_ops.divide(k_ops.subtract(pt_out, k_ops.mean(pt_out)), k_ops.std(pt_out))
+            return pt_out_std
 
     dataset = load_dataset(attribute)
-    box_cox_model = BoxCoxModel()
+    box_cox_model = BoxCoxModel(lmbda=BOX_COX_LAMBDA)
     box_cox_model.trainable = False
     box_cox_model.compile(run_eagerly=True)  # TODO - fix pt exception handling to run with autograph
     power_transform_attribute = box_cox_model.predict(dataset)
-    hist_output_path = HIST_OUTPUT_PATH / "box-cox"
-    plot_distribution(power_transform_attribute, hist_output_path, attribute_name)
+    plot_distribution(power_transform_attribute, HIST_OUTPUT_PATH, "box-cox", BOX_COX_LAMBDA, attribute_name)
 
 
 def test_yeo_johnson_transform(attribute: str):
     class YeoJohnsonModel(keras.Model):
 
+        def __init__(self, lmbda: int):
+            super(YeoJohnsonModel, self).__init__()
+            self._lmbda = lmbda
+
         # noinspection PyAttributeOutsideInit
         def build(self, input_shape):
-            self._yeo_johnson_layer = YeoJohnson(lambda_init=-2.22, batch_norm=keras.layers.BatchNormalization())
+            self._yeo_johnson_layer = YeoJohnson(lambda_init=self._lmbda)
 
         def call(self, inputs):
-            return self._yeo_johnson_layer(inputs)
+            pt_out = self._yeo_johnson_layer(inputs)
+            pt_out_std = k_ops.divide(k_ops.subtract(pt_out, k_ops.mean(pt_out)), k_ops.std(pt_out))
+            return pt_out_std
 
     dataset = load_dataset(attribute)
-    yeo_johnson_model = YeoJohnsonModel()
+    yeo_johnson_model = YeoJohnsonModel(lmbda=YEO_JOHNSON_LAMBDA)
     yeo_johnson_model.trainable = False
     yeo_johnson_model.compile(run_eagerly=True)  # TODO - fix pt exception handling to run with autograph
     power_transform_attribute = yeo_johnson_model.predict(dataset)
-    hist_output_path = HIST_OUTPUT_PATH / "yeo-johnson"
-    plot_distribution(power_transform_attribute, hist_output_path, attribute_name)
+    plot_distribution(power_transform_attribute, HIST_OUTPUT_PATH, "yeo-johnson", YEO_JOHNSON_LAMBDA, attribute_name)
 
 
 if __name__ == '__main__':

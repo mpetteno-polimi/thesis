c9c1e3850cb5f30a1fa8bef68746accae2e04e1f
Matteo Pettenò <matteo.petteno@mail.polimi.it>
2024-05-04 15:37:19 +0200

    + flags for python snappy script

--- setup_conda_env.sh
+++ setup_conda_env.sh
@@ -6,6 +6,9 @@ CONDA_PREFIX=./venv
 eval "$(conda shell.bash deactivate)"
 conda env remove --prefix $CONDA_PREFIX
 
+# Flags to C++ compiler (needed to install python-snappy package)
+export CPPFLAGS="-I/opt/homebrew/include -L/opt/homebrew/lib"
+
 # Create new environment
 conda env create --prefix $CONDA_PREFIX --file environment.yml
 conda env update --prefix $CONDA_PREFIX --file environment.local.yml
